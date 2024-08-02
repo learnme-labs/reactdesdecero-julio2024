43dfe7b0690d83c408ac096bef230b0e70b48d4d
GIT Teacher <oscar.marq.olv@gmail.com>
2024-08-02 14:14:41 -0600

    Completando sistema de rutas e iniciando sistema de navegacion
     Changes to be committed:
    	modified:   tecgurus-task/src/App.css
    	modified:   tecgurus-task/src/App.js

--- tecgurus-task/src/App.js
+++ tecgurus-task/src/App.js
@@ -1,7 +1,7 @@
 // src/App.js
 
 import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, NavLink, Routes, Route } from 'react-router-dom';
 
 import PageHome from './components/page/home';
 import PageLogin from './components/page/auth';
@@ -11,21 +11,53 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
+  // sistema de rutas básico
+  /*return (
     <>
       <BrowserRouter>
         <Routes>
           <Route path="/"  element={ <PageHome /> }></Route>
           <Route path="/login" element={ <PageLogin /> } ></Route>
           <Route path="/setting" element={ <></> } ></Route>
-
-
           <Route path="*" element={ <PageError /> } ></Route>
           
           {
           // <Route path="/*" element={ <PageLogin /> } ></Route>          /login  /signup  /home /new /setting
           }
         </Routes>
+      </BrowserRouter>
+    </>
+  );*/
+
+  // sistema de rutas con navegacion
+  return (
+    <>
+      <BrowserRouter>
+        <div>
+          <nav className='menu'>
+            <ul>
+              <li>
+                <NavLink to="/" exact activeClassName="active">Home</NavLink>
+              </li>
+              <li>
+                <NavLink to="/login">Login</NavLink>
+              </li>
+              <li>
+                <NavLink></NavLink>
+              </li>
+            </ul>
+          </nav>
+
+          <Routes>
+            <Route path="/" element={ <PageHome />}></Route>
+            <Route path="/login" element={ <PageLogin />}></Route>
+            <Route path="*" element={ <PageError />}></Route>
+          </Routes>
+
+
+
+
+        </div>
       </BrowserRouter>
     </>
   );
